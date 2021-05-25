fee382afcaff8102aaa4d90b6c75a78762c7f553
jessijoke <jessijoke@yahoo.com>
2021-05-25 03:22:11 -0400

    commit tree 932b4b222d5d00c65d930b094a001f3de3009b70

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++ ) {
+        newArr.push(-1 * arr[i])
+      }
+    return newArr;
+}
+
+function mapToNoChange(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i])
+    }
+    return newArr;
+}
+
+function mapToDouble(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2)
+    }
+    return newArr;
+}
+
+function mapToSquare(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * arr[i])
+    }
+    return newArr;
+}
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
+  }
